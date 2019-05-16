54f4b1ddfa5bfeca07ecde7e28a34172b37a3684
xiaoxiaoflowernutwall <49676919+xiaoxiaoflowernutwall@users.noreply.github.com>
2019-05-16 20:30:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,70 @@ var app = Express();
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({	extended: true}));
 
-app.get("/hi",(request, response) =>{
-	response.send("Hello world");
+app.get("/notes",(request, response) =>{
+
+	MongoClient.connect(CONNECTION_URL,{useNewUrlParser: true},(error,client)=>{
+		if (error) {
+			response.send(error);
+			throw error;
+		}
+		database = client.db(DATABSE_NAME);
+		connection = database.collection("Notes");
+
+		collection.find({}).toArray((error,result)=>{
+			if (error) {
+				return response.statue.send(error);
+			}
+			response.send(result);
+		});
+	});
+});
+
+app.get("/notes/:id",(request, response) => {
+
+	MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error,client)=>{
+		if (error) {
+			response.send(error);
+			throw error;
+		}
+		database = client.db(DATABSE_NAME);
+		collection = database.connection("Notes");
+
+		connection.find({}).toArray((error, result)=>{
+			if (error) {
+				return response.statue(500).send(error);			
+			}
+
+			var numberID = parseInt(request.params.id);
+
+			if(numberID >= result.length)
+				response.send("Not enough elements in database")
+			else
+				response.send(result[numberID]);
+		});
+	});
+});
+
+const CONNECTION_URL = "mongodb+srv://admin:<password>@cluster0-vidvh.mongodb.net/test?retryWrites=true";
+const DATABSE_NAME = "Clusters0";
+
+app.post("/notes",(request,response) =>{
+
+	MongoClient.connect(CONNECTION_URL,{useNewUrlParser:true},(error,client)=>{
+		if(error) {
+			response.send(error);
+			throw error;
+		}
+		database = client.db(DATABSE_NAME);
+		connection = database.connection("Notes");
+
+		connection.insert(request.body,(error,result) =>{
+			if(error) {
+				return response.statue(500).send(error);
+			}
+			response.send(result.result);
+		});
+	});
 });
 
 module.exports = app;
